bc3b012deaee71a73173723a7e618c6acffd085d
Noah Yetter <nyetter@gmail.com>
2020-12-12 16:28:06 -0700

    adjust sql 8p1 to test an approach for 8p2

--- 08/sql_solution.sql
+++ 08/sql_solution.sql
@@ -36,6 +36,7 @@ with recursive execution( line_num
                         , next_line_num
                         , executed_lines
                         , counter
+                        , program_state
                         )
 as
 (
@@ -47,6 +48,7 @@ as
          , 1 as NEXT_LINE_NUM
          , ',' as EXECUTED_LINES
          , 0 as COUNTER
+         , 'normal' as PROGRAM_STATE
     --
     union all
     --
@@ -67,10 +69,15 @@ as
                         ) as NEXT_LINE_NUM
          , (e.executed_lines || i.line_num::text || ',') as EXECUTED_LINES
          , e.counter + 1 as COUNTER
+         , case when e.executed_lines like ('%,' || i.line_num::text || ',%')
+                then 'looped'
+                else 'normal'
+                 end as PROGRAM_STATE --this lets us get one more row after finding the loop -- or termination in p2 -- so we can report on how we stopped
       from execution e
       join advent2020.input08 i on(e.next_line_num = i.line_num)
      where 1=1
-       and e.executed_lines not like ('%,' || i.line_num::text || ',%')
+       --and e.executed_lines not like ('%,' || i.line_num::text || ',%') --this is cleaner but makes it hard to show the terminal state
+       and e.program_state = 'normal'
 )
 , part1_base as
 (
@@ -79,6 +86,7 @@ as
          , instruction
          , argument
          , accumulator
+         , program_state
       from execution
      where 1=1
      order by counter
@@ -89,16 +97,73 @@ as
          , accumulator
       from part1_base
      where 1=1
-       and counter = (select max(counter) from part1_base)
+       and counter = (select max(counter) from part1_base where program_state = 'normal')
     --answer: 2034
 )
+select * from part1_answer
 ;
 
 /*
-select i.line_num
-     , i.instruction
-     , i.argument
-  from advent2020.input08 i
+select p.line_num
+     , p.instruction
+     , p.argument
+     , 
+  from (
+        select p.line_num
+             , (case p.instruction
+                when 'jmp' then 'nop'
+                when 'nop' then 'jmp'
+                 end) as INSTRUCTION
+             , p.argument
+          from advent2020.input08
+         where 1=1
+           and p.instruction in ( 'jmp', 'nop' )
+       ) patched
+  join lateral (
+                with recursive execution( line_num
+                                        , instruction
+                                        , argument
+                                        , accumulator
+                                        , next_line_num
+                                        , executed_lines
+                                        , counter
+                                        )
+                as
+                (
+                    select 0 as LINE_NUM
+                         , 'nop' as INSTRUCTION
+                         , 0 as ARGUMENT
+                         , 0 as ACCUMULATOR
+                         , 1 as NEXT_LINE_NUM
+                         , ',' as EXECUTED_LINES
+                         , 0 as COUNTER
+                    --
+                    union all
+                    --
+                    select i.line_num
+                         , i.instruction
+                         , i.argument
+                         , e.accumulator + (case i.instruction
+                                            when 'acc' then i.argument
+                                            when 'jmp' then 0
+                                            when 'nop' then 0
+                                             end
+                                           ) as ACCUMULATOR
+                         , i.line_num + (case i.instruction
+                                         when 'acc' then 1
+                                         when 'jmp' then i.argument
+                                         when 'nop' then 1
+                                          end
+                                        ) as NEXT_LINE_NUM
+                         , (e.executed_lines || i.line_num::text || ',') as EXECUTED_LINES
+                         , e.counter + 1 as COUNTER
+                      from execution e
+                      join advent2020.input08 i on(e.next_line_num = i.line_num)
+                     where 1=1
+                       and e.executed_lines not like ('%,' || i.line_num::text || ',%')     
+                )
+                    
+               ) foo on(true)
  where 1=1
    and i.instruction in ( 'jmp', 'nop' )
  order by i.line_num
