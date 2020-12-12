eddff000d226522012db13cf39106c756f917dbc
Noah Yetter <noah@sympoz.com>
2020-12-12 11:50:57 -0700

    clojure solution to #8 part 1

---
+++ 08/clojure-solution.clj
@@ -0,0 +1,102 @@
+;For example, consider the following program:
+;
+;nop +0
+;acc +1
+;jmp +4
+;acc +3
+;jmp -3
+;acc -99
+;acc +1
+;jmp -4
+;acc +6
+
+;First, the nop +0 does nothing. Then, the accumulator is increased from 0 to 1 (acc +1) 
+;and jmp +4 sets the next instruction to the other acc +1 near the bottom. After it 
+;increases the accumulator from 1 to 2, jmp -4 executes, setting the next instruction to 
+;the only acc +3. It sets the accumulator to 5, and jmp -3 causes the program to continue 
+;back at the first acc +1.
+
+;This is an infinite loop: with this sequence of jumps, the program will run forever. 
+;The moment the program tries to run any instruction a second time, you know it will never terminate.
+
+;Immediately before the program would run an instruction a second time, 
+;the value in the accumulator is 5.
+
+;Run your copy of the boot code. Immediately before any instruction is executed a second time, 
+;what value is in the accumulator?
+
+
+(ns net.blergh.advent2020
+    (:require [clojure.string :as str]
+              [clojure.edn :as edn]
+    )
+)
+
+(def input08 (slurp "input08"))
+
+(def input-lines (str/split-lines input08))
+
+(def input-code 
+    (vec
+        (for [line input-lines]
+            (vec (str/split line #" "))
+        )
+    )
+)
+
+(def sample-input
+"nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6"
+)
+(def sample-code
+    (vec
+        (for [line (str/split-lines sample-input)]
+            (vec (str/split line #" "))
+        )
+    )
+)
+;(println code)
+
+(def code input-code)
+
+; closes over "code" instead of taking it as an argument, since it doesn't mutate
+(defn execute-until-repeat [instr-num accumulator already-run-instr-nums]
+    (if (contains? already-run-instr-nums instr-num)
+        accumulator
+        (let [ [instruction argument-string] (get code instr-num)
+             , argument (edn/read-string argument-string)
+             ]
+             ;(println (str "instruction=" instruction ", argument=" argument ", accumulator=" accumulator))
+             (cond
+                 (= instruction "acc")  (execute-until-repeat 
+                                            (+ instr-num 1)
+                                            (+ accumulator argument)
+                                            (conj already-run-instr-nums instr-num)
+                                        )
+                 (= instruction "jmp")  (execute-until-repeat
+                                            (+ instr-num argument)
+                                            accumulator
+                                            (conj already-run-instr-nums instr-num)
+                                        )
+                 (= instruction "nop")  (execute-until-repeat
+                                            (+ instr-num 1)
+                                            accumulator
+                                            (conj already-run-instr-nums instr-num)
+                                        )
+                 :else (throw (Exception. "UNKNOWN INSTRUCTION"))
+             )
+        )
+    )
+)
+
+(def result (execute-until-repeat 0 0 #{}))
+(println (str "(p1 answer) accumulator at first repeat = " result))
+;answer=2034
+
