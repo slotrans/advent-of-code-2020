bb4c8511ce75ecb6563717bd2e6e334204a8895c
Noah Yetter <noah@sympoz.com>
2020-12-13 11:08:21 -0700

    sql solution to 9p2

--- 09/sql_solution.sql
+++ 09/sql_solution.sql
@@ -56,6 +56,54 @@ with validity_check as
        and is_valid = false
      order by idx
      limit 1
+    --answer: 258585477 (idx=594)
 )
 select * from part1_answer
+;
+
+
+with var as
+(
+    select 258585477 as TARGET_VAL
+         , 594 as TARGET_IDX
+)
+, input_subset as
+(
+    select i.idx
+         , i.val
+      from advent2020.input09 i
+     where 1=1
+       and i.idx < (select target_idx from var)
+)
+, part2_base as
+(
+    select i.idx
+         , i.val
+         , range.idx as R_IDX
+         , range.r_sum
+         , range.r_min
+         , range.r_max
+         , range.r_array
+      from input_subset i
+      join lateral (select j.idx
+                         , sum(j.val) over(order by j.idx rows between unbounded preceding and current row) as R_SUM
+                         , array_agg(j.val) over(order by j.idx rows between unbounded preceding and current row) as R_ARRAY
+                         , min(j.val) over(order by j.idx rows between unbounded preceding and current row) as R_MIN
+                         , max(j.val) over(order by j.idx rows between unbounded preceding and current row) as R_MAX
+                      from input_subset j
+                     where 1=1
+                       and j.idx >= i.idx
+                   ) range on(range.r_sum = (select target_val from var))
+     where 1=1
+       and i.idx >= 2 --need at least 2 in range
+     order by i.idx
+     limit 1
+)
+select 'part2'
+     , r_min + r_max
+  from part2_base
+ where 1=1
+--answer range {13858643,9455395,9908827,16794010,13221299,11563238,12646458,11137204,11774548,12220424,14302571,14304519,14748447,25865809,22680253,16578014,27525818}
+--min 9455395 + max 27525818
+--answer: 36981213
 ;
