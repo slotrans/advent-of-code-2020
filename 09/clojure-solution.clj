1a86db8ea228c78ef09b63b88c96079098814215
Noah Yetter <nyetter@gmail.com>
2020-12-12 18:05:51 -0700

    very start of a clojure solution to 9p1

---
+++ 09/clojure-solution.clj
@@ -0,0 +1,60 @@
+
+(ns net.blergh.advent2020
+    (:require [clojure.string :as str]
+    )
+)
+
+
+(def sample-input
+"35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576")
+
+(def sample-stream 
+    (vec
+        (for [line (str/split-lines sample-input)]
+            (Integer/parseInt line)
+        )
+    )
+)
+
+(println sample-stream)
+
+(defn is-valid? [number window]
+    (some
+        (fn [x] (= x number))
+        (for [ x window
+             , y window
+             :when (not (= x y))
+             ]
+             (+ x y)
+        )
+    )
+)
+
+(println (is-valid? 40 [35 20 15 25 47]))
+
+(println (is-valid? 62 [20 15 25 47 40]))
+
+(println (is-valid? 127 [95 102 117 150 182]))
+
+;(defn find-first-invalid
+;    
+;)
