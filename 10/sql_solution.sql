3b66c4a1a0131aa5d0402c7af4faa2c8ad28045f
Noah Yetter <nyetter@gmail.com>
2020-12-19 11:34:51 -0700

    sql solution to 10p2

--- 10/sql_solution.sql
+++ 10/sql_solution.sql
@@ -21,7 +21,7 @@ COPY 94
 
 with extended as
 (
-    select 0 as joltage
+    select 0 as JOLTAGE
     union all
     (
     select val
@@ -62,5 +62,54 @@ with extended as
          , (select _count from diff_counts where difference = 1) * (select _count from diff_counts where difference = 3) as ANSWER
     --answer: 1914
 )
+, filled as
+(
+    select series.val as STEP
+         , e.joltage
+      from generate_series( (select min(joltage) from extended)
+                          , (select max(joltage) from extended)
+                          ) as series(val)
+      left join extended e on(series.val = e.joltage)
+     where 1=1
+)
+, part2_base as 
+(
+    --the "normal language" solutions accumulate all adapter->ways pairs in a map, whereas this carries through 
+    --only the needed pairs, in columns
+    with recursive stairs(step, joltage, ways, ways_lag1, ways_lag2) as
+    (
+        select 0 as STEP
+             , 0 as JOLTAGE
+             , 1::bigint as WAYS
+             , 0::bigint as WAYS_LAG1
+             , 0::bigint as WAYS_LAG2
+        --
+        union all
+        --
+        select f.step
+             , f.joltage
+             , case when f.joltage is not null
+                    --then (s.ways_lag1 + s.ways_lag2 + s.ways_lag3) 
+                    then (s.ways + s.ways_lag1 + s.ways_lag2)
+                    else 0::bigint
+                     end as WAYS
+             , s.ways as WAYS_LAG1
+             , s.ways_lag1 as WAYS_LAG2
+          from stairs s 
+          join filled f on(s.step + 1 = f.step)
+         where 1=1
+    )
+    select * from stairs
+)
+, part2_answer as
+(
+    select 'part 2'
+         , max(ways) 
+      from part2_base
+     where 1=1
+    --answer: 9256148959232
+)
 select * from part1_answer
+union all
+select * from part2_answer
 ;
